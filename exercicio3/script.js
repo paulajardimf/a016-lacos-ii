3fa18b06bf851cb1212df7befd24fb2443f5149b
Paula <paulajardimf@gmail.com>
2022-09-13 15:35:11 -0300

    exercício revisado

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,5 +1,5 @@
 const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"];
 
-for (paises of maioresPaises) {
-  console.log(`${maioresPaises.indexOf(paises) + 1} - ${paises}`);
+for (pais of maioresPaises) {
+  console.log(`${maioresPaises.indexOf(pais) + 1} - ${pais}`);
 }
